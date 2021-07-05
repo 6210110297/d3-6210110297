035148b70d81c38afaf485ce1d86aba292363532
rachanon khongchai <6210110297@psu.ac.th>
2021-07-05 20:18:30 +0700

    try to using

--- bin/d3.dart
+++ bin/d3.dart
@@ -1,3 +1,8 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  var input = 'on 18 0,set,inc,set,inc,set,inc,inc,set';
+  var commandList = input.split(',');
+  commandList.forEach((command) {
+    var atList = command.split(' ');
+    if (atList.length == 0) {}
+  });
 }
