f74fbb1578985108b7deb8669b04a8d11a65eb4a
rachanon khongchai <6210110297@psu.ac.th>
2021-07-05 20:18:43 +0700

    another one using

--- bin/clock_state_manage.dart
+++ bin/clock_state_manage.dart
@@ -7,17 +7,23 @@ class ClockStateManage {
     SetMinClockState()
   ];
 
-  int currentState = 0;
+  int currentStateIndex = 0;
+
+  ClockState state = NormalClockState();
 
   final NormalClockState clock = NormalClockState();
 
   ClockStateManage({hour, min}) {
     clock.hour = hour ?? clock.hour;
     clock.min = min ?? clock.min;
+    if (currentStateIndex == 0 && clock.hour == 0 && clock.min == 0) {
+      currentStateIndex = 1;
+    }
   }
 
-  ClockState nextState() {
-    currentState = (currentState + 1) % 3;
-    return states[currentState];
+  ClockState set() {
+    currentStateIndex = (currentStateIndex + 1) % 3;
+    state = states[currentStateIndex];
+    return state;
   }
 }
