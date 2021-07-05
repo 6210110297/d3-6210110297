1ffc08cd9539bb69cdfa747fb9b21a3ffd570ae8
rachanon khongchai <6210110297@psu.ac.th>
2021-07-05 20:39:07 +0700

    fix bug

--- bin/clock_state.dart
+++ bin/clock_state.dart
@@ -1,7 +1,6 @@
 abstract class ClockState {
   int? hour;
   int? min;
-  final NormalClockState clock = NormalClockState();
   ClockState({this.hour = 0, this.min = 0});
   void incTime();
 }
@@ -20,7 +19,7 @@ class SetHourClockState extends ClockState {
   SetHourClockState({hour, min}) : super(hour: hour, min: min);
   @override
   void incTime() {
-    clock.hour = (clock.hour! + 1) % 24;
+    hour = (hour! + 1) % 24;
   }
 }
 
@@ -28,6 +27,6 @@ class SetMinClockState extends ClockState {
   SetMinClockState({hour, min}) : super(hour: hour, min: min);
   @override
   void incTime() {
-    clock.min = (clock.min! + 1) % 60;
+    min = (min! + 1) % 60;
   }
 }
--- bin/d3.dart
+++ bin/d3.dart
@@ -1,8 +1,25 @@
+import 'clock_state_manage.dart';
+
 void main(List<String> arguments) {
   var input = 'on 18 0,set,inc,set,inc,set,inc,inc,set';
   var commandList = input.split(',');
-  commandList.forEach((command) {
-    var atList = command.split(' ');
-    if (atList.length == 0) {}
-  });
+  var atList = commandList.first.split(' ');
+
+  print('${atList[2]}');
+  final ClockStateManage manage;
+  if (atList[0] == 'on') {
+    manage =
+        ClockStateManage(hour: int.parse(atList[1]), min: int.parse(atList[2]));
+  } else {
+    manage = ClockStateManage(hour: 0, min: 0);
+  }
+  // commandList.forEach((command) {
+  //   if (command == 'set') {
+  //     manage.set();
+  //   } else if (command == 'inc') {
+  //     manage.state.incTime();
+  //   }
+  // });
+
+  print('Now, It is ${manage.state.hour}:${manage.state.min}');
 }
